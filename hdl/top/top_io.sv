f45c9793dafab52177aa53b46ea83a4ad46d97b3
Tony <tonydgoodwin@gmail.com>
2025-09-18 15:35:12 -0400

    timing failure check

--- hdl/top/top_io.sv
+++ hdl/top/top_io.sv
@@ -1,10 +1,8 @@
 module top_io (
-  input       SYSCLK_N    ,
-  input       SYSCLK_P    ,
-  output      LED_RGB_R2  ,
-  output      LED_RGB_G2  ,
-  output      LED_RGB_B2  ,
-  input [1:0] GPIO_SW
+  input         SYSCLK_N    ,
+  input         SYSCLK_P    ,
+  output [3:0]  LEDS        ,
+  input [1:0]   GPIO_SW
 );
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 
@@ -18,21 +16,49 @@ module top_io (
     .clk_in1_n  (SYSCLK_N   )   // input clk_in1_n
   );
 
-
-
-
   logic led=0;
-  logic [15:0] cnt='0;
+  logic [31:0] cnt='0;
 
   always_ff @(posedge clk100) begin
     cnt <= cnt + 1;
-    if (cnt == 16'hFFFF) led <= ~led;  
+    if (cnt == 32'hFFFFFFFF) led <= ~led;  
   end
 
 
-  assign LED_RGB_R2 = led;
-  assign LED_RGB_G2 = led;
-  assign LED_RGB_B2 = led;
+  long_comb_path long_comb_path_inst1 (
+    .rst (),
+    .clk (clk100),
+    .d_i (cnt),
+    .d_o (LEDS[0])
+  );
+
+  long_comb_path long_comb_path_inst2 (
+    .rst (),
+    .clk (clk200),
+    .d_i (cnt),
+    .d_o (LEDS[1])
+  );
+
+  long_comb_path long_comb_path_inst4 (
+    .rst (),
+    .clk (clk400),
+    .d_i (cnt),
+    .d_o (LEDS[2])
+  );
+
+  long_comb_path long_comb_path_inst6 (
+    .rst (),
+    .clk (clk600),
+    .d_i (cnt),
+    .d_o (LEDS[3])
+  );
+
+
+
+
+//  assign LED_RGB_R2 = led;
+//  assign LED_RGB_G2 = led;
+//  assign LED_RGB_B2 = led;
 
 
 endmodule
--- xdc/pins.xdc
+++ xdc/pins.xdc
@@ -3,16 +3,17 @@
 
 #set_property BITSTREAM.CONFIG.USR_ACCESS TIMESTAMP [current_design]
 
-set_property PACKAGE_PIN G24   [get_ports {SYSCLK_P}]; # AC19, 
-set_property PACKAGE_PIN G25   [get_ports {SYSCLK_N}]; # AD19, 
+set_property PACKAGE_PIN H26   [get_ports {SYSCLK_P}]; # G24
+set_property PACKAGE_PIN G25   [get_ports {SYSCLK_N}]; # G25
 set_property IOSTANDARD LVDS    [get_ports {SYSCLK*}];
 
 
 
-set_property PACKAGE_PIN E26     [get_ports {LED_RGB_R2}]; # AE18, G26
-set_property PACKAGE_PIN H26     [get_ports {LED_RGB_G2}]; # AF20, 
-set_property PACKAGE_PIN J26     [get_ports {LED_RGB_B2}]; # AC16, 
-set_property IOSTANDARD LVCMOS12  [get_ports {LED_RGB*}];
+set_property PACKAGE_PIN E26     [get_ports {LEDS[0]}]; # 
+set_property PACKAGE_PIN H26     [get_ports {LEDS[1]}]; # 
+set_property PACKAGE_PIN J26     [get_ports {LEDS[2]}]; # 
+set_property PACKAGE_PIN K28     [get_ports {LEDS[3]}]; #  
+set_property IOSTANDARD LVCMOS12  [get_ports {LEDS*}];
 
 # gpio_sw_tri_i 
 set_property PACKAGE_PIN K26     [get_ports {GPIO_SW[1]}]; # AB21, 
